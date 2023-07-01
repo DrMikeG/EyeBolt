d36b2d9f16ebd30c22a85dbd93d97ea748f9e4fd
Mike Gibbens <mike.gibbens@siemens.com>
2023-07-01 19:58:20 +0100

    Using registers to change size

--- Mk5/Code/12_TweakCameraSettings/my_camera_changeSize.py
+++ Mk5/Code/12_TweakCameraSettings/my_camera_changeSize.py
@@ -13,6 +13,70 @@ def change_setting(cam):
     cam.size=adafruit_ov5640.OV5640_SIZE_HD
     # self._size = size
     # self._set_size_and_colorspace()
+    
+    #def _set_size_and_colorspace(self) -> None:  # pylint: disable=too-many-locals
+    #size = self._size
+    _ASPECT_RATIO_16X9 = 4
+    width, height, ratio = [1280, 720, _ASPECT_RATIO_16X9]
+    _w = width
+    _h = height
+    (
+        max_width,
+        max_height,
+        start_x,
+        start_y,
+        end_x,
+        end_y,
+        offset_x,
+        offset_y,
+        total_x,
+        total_y,
+    ) = [2560, 1440, 0, 240, 2623, 1711, 32, 16, 2844, 1488] # 16x9
+
+    _binning = (width <= max_width // 2) and (height <= max_height // 2)
+    _scale = not (
+        (width == max_width and height == max_height)
+        or (width == max_width // 2 and height == max_height // 2)
+    )
+
+    _X_ADDR_ST_H = 0x3800    
+    _X_ADDR_END_H = 0x3804
+    _X_OUTPUT_SIZE_H = 0x3808
+    _X_TOTAL_SIZE_H = 0x380C
+    _X_OFFSET_H = 0x3810
+    _ISP_CONTROL_01 = 0x5001
+
+    cam._write_addr_reg(_X_ADDR_ST_H, start_x, start_y)
+    cam._write_addr_reg(_X_ADDR_END_H, end_x, end_y)
+    cam._write_addr_reg(_X_OUTPUT_SIZE_H, width, height)
+
+    if not _binning:
+        cam._write_addr_reg(_X_TOTAL_SIZE_H, total_x, total_y)
+        cam._write_addr_reg(_X_OFFSET_H, offset_x, offset_y)
+    else:
+        if width > 920:
+            cam._write_addr_reg(_X_TOTAL_SIZE_H, total_x - 200, total_y // 2)
+        else:
+            cam._write_addr_reg(_X_TOTAL_SIZE_H, 2060, total_y // 2)
+        cam._write_addr_reg(_X_OFFSET_H, offset_x // 2, offset_y // 2)
+
+    cam._write_reg_bits(_ISP_CONTROL_01, 0x20, cam._scale)
+
+    # This sets binning and flipping and a few other things
+    cam._set_image_options()
+
+    #if cam.colorspace == adafruit_ov5640.OV5640_COLOR_JPEG:
+        #sys_mul = 200
+        #if cam.size < adafruit_ov5640.OV5640_SIZE_QVGA:
+        #    sys_mul = 160
+        #if cam.size < adafruit_ov5640.OV5640_SIZE_XGA:
+        #    sys_mul = 180
+        #cam._set_pll(False, sys_mul, 4, 2, False, 2, True, 4)
+    #else:
+    #    cam._set_pll(False, 32, 1, 1, False, 1, True, 4)
+
+    # hopefully none of this needs to change
+    #cam._set_colorspace()
 
 
 def init_camera():
