6167060c99169733894be40167b4cf982adea830
Mike Gibbens <mike.gibbens@siemens.com>
2023-07-03 22:54:26 +0100

    960x960 works

--- Mk5/Code/12_TweakCameraSettings/code.py
+++ Mk5/Code/12_TweakCameraSettings/code.py
@@ -11,11 +11,11 @@ import digitalio
 from digitalio import DigitalInOut, Direction, Pull
 
 import mount_sd
-from my_camera import init_camera, change_setting
+from my_camera import init_camera
 from my_motor import init_stepper_motor, stepper_fwd
 
 
-print("Take 2 photos, changing the setting in between")
+print("Take 1 photos, testing crop settings")
 
 def init_push_button():
     button_pin = board.GP6
@@ -55,7 +55,7 @@ def get_next_run_path():
 
 def main():
 
-    cam = init_camera()
+    cam = init_camera(True)
 
     if cam is None:
         raise SystemExit("Camera not initialized")
@@ -64,6 +64,7 @@ def main():
 
     button = init_push_button()
 
+    time.sleep(1)
     print("init complete")
     
     width = cam.width
@@ -102,44 +103,22 @@ def main():
             cam.capture(buffer)
             jpgPath = dirPath + "/capture_0.jpg"
             print("Writing image file {}".format(jpgPath))
+
+            total_bytes = len(buffer)
+            print("Saving {} bytes".format(total_bytes))
+            progress_interval = total_bytes // 20  # Update progress every 5%
+            progress_list = [i * progress_interval for i in range(1, 22)]  # List representing 5%, 10%, 15%, ...
+            print("....................")
             with open(jpgPath, "w") as f:
+                bytes_written = 0
                 for byte in buffer:
                     f.write(byte.to_bytes(1, "big"))
-            
-            # Change settings
-            change_setting(cam)
-            time.sleep(1)
-
-            if True:
-                width = cam.width
-                height = cam.height
-                print("Image width {} height {}".format(width, height))
-                newBufSize = cam.capture_buffer_size
-                print("Image bytes {}".format(newBufSize))     
-                buffer2 = bytearray(newBufSize)       
-                quality = cam.quality
-                print("Image compression factor {}".format(quality))
-
-                if buffer2 is None:
-                    raise SystemExit("Could not allocate a bitmap")
-
-                print("Stablise white balance")
-                for _ in range(10):
-                    print(".", end='')
-                    cam.capture(buffer2)
-                    time.sleep(0.1)
-                print("\n", end='')
-                time.sleep(1)
-
-                # Capture image with modified settings
-                print("Taking picture 1")
-                cam.capture(buffer2)
-                jpgPath = dirPath + "/capture_1.jpg"
-                print("Writing image file {}".format(jpgPath))
-                with open(jpgPath, "w") as f:
-                    for byte in buffer2:
-                        f.write(byte.to_bytes(1, "big"))
+                    bytes_written += 1
+                    if bytes_written >= progress_list[0]:
+                        print("#", end="")
+                        progress_list.pop(0)
             break
+            print("")
         
     time.sleep(0.1)  # Small delay to debounce the button
 
--- Mk5/Code/12_TweakCameraSettings/my_camera_changeSize.py
+++ Mk5/Code/12_TweakCameraSettings/my_camera_changeSize.py
@@ -4,62 +4,111 @@ import board
 import digitalio
 import time
 
-def change_setting(cam):
+def init_camera(useCrop):
 
-    
-    print("Overriding defaults")
-    # These values are valid - which means the mx and tx don't need to change to 
-    # allow scaling
-    # I think the ratios need to be correct though, in this cases I went 1/10th
-    adafruit_ov5640._resolution_info[adafruit_ov5640.OV5640_SIZE_HD] = [240, 240, 7]  # 240x240
-    # [1280, 720, _ASPECT_RATIO_16X9],  # HD
-    # [2560, 1440, 0, 240, 2623, 1711, 32, 16, 2844, 1488],  # 16x9
-    
-    adafruit_ov5640._ratio_table[7] = [1920, 1920, 320, 0, 2543, 1951, 32, 16, 2684, 1968]  # 1x1
+    if useCrop:
+        print("Overriding defaults")
+        # These values are valid - which means the mx and tx don't need to change to 
+        # allow scaling
+        # I think the ratios need to be correct though, in this cases I went 1/10th
+        adafruit_ov5640._resolution_info[adafruit_ov5640.OV5640_SIZE_HD] = [960, 960, 7]  # 240x240
+        # [1280, 720, _ASPECT_RATIO_16X9],  # HD
+        # [2560, 1440, 0, 240, 2623, 1711, 32, 16, 2844, 1488],  # 16x9
+        adafruit_ov5640._ratio_table[7] = [1920, 1920, 320, 0, 2543, 1951, 32, 16, 2684, 1968]  # 1x1
 
-    print("Changing camera size")
-    cam.size = adafruit_ov5640.OV5640_SIZE_HD
-    #cam.size = adafruit_ov5640.OV5640_SIZE_240X240
-    #cam.colorspace = adafruit_ov5640.OV5640_COLOR_GRAYSCALE
-    time.sleep(1)
-    print("New settings")
-    time.sleep(1)
-    # Can I access the resolution_info?
+
+    # Change the values in row 0 of the _resolution_info table
+    #adafruit_ov5640._resolution_info[0] = [2560,1400,8]  # Assign new values to row 0
+    # Change the values in row 0 of the _ratio_table table
+
+    #  mw,   mh,  sx,  sy,   ex,   ey, ox, oy,   tx,   ty
+    #adafruit_ov5640._ratio_table[0] =  [1920, 1920, 320, 0, 2543, 1951, 32, 16, 2684, 1968]  # 1x1
+    #adafruit_ov5640._ratio_table[0] = [2650, 1820, 850, 500, 1800, 1400, 32, 16, 2844, 1868]  # Assign new values to row 0
+
+    print("initialising Camera")
+    print("construct bus")
+    bus = busio.I2C(board.GP9, board.GP8)
+    print("construct camera")
+    reset = digitalio.DigitalInOut(board.GP10)
+    cam = adafruit_ov5640.OV5640(
+    bus,
+    data_pins=(
+        board.GP12,
+        board.GP13,
+        board.GP14,
+        board.GP15,
+        board.GP16,
+        board.GP17,
+        board.GP18,
+        board.GP19,
+    ),
+    clock=board.GP11,
+    vsync=board.GP7,
+    href=board.GP21,
+    mclk=board.GP20,
+    shutdown=None,
+    reset=reset,
+    size=adafruit_ov5640.OV5640_SIZE_HD
+    )
+    print("print chip id")
+    print(cam.chip_id)
+
+    #OV5640_SIZE_SVGA = 9  # 800x600
+    #OV5640_SIZE_XGA = 10  # 1024x768
+    #OV5640_SIZE_HD = 11  # 1280x720
+    #OV5640_SIZE_SXGA = 12  # 1280x1024
+    #OV5640_SIZE_UXGA = 13  # 1600x1200
+    #OV5640_SIZE_QHDA = 14  # 2560x1440
+    #OV5640_SIZE_WQXGA = 15  # 2560x1600
+    #OV5640_SIZE_PFHD = 16  # 1088x1920
+    #OV5640_SIZE_QSXGA = 17  # 2560x1920
+
+    # This sets the color format for the camera to RGB.
+    # The constant adafruit_ov5640.OV5640_COLOR_RGB specifies the byte ordering and format of the color data that the OV5640 camera outputs.
+    # In this case, the format is RGB with little-endian byte ordering.
+    #cam.colorspace = adafruit_ov5640.OV5640_COLOR_RGB
+    
+    # 2560x1440 @ 24 seems like a good starting point
+
+    # Use JPG
+    cam.quality = 24
+    cam.colorspace = adafruit_ov5640.OV5640_COLOR_JPEG 
+
+    # Use RGB
+    #cam.colorspace = adafruit_ov5640.OV5640_COLOR_JPEG 
+    
+    cam.effect = adafruit_ov5640.OV5640_SPECIAL_EFFECT_NONE
+    #cam.effect = adafruit_ov5640.OV5640_SPECIAL_EFFECT_NEGATIVE
+    cam.white_balance = adafruit_ov5640.OV5640_WHITE_BALANCE_AUTO
+
+    #OV5640_WHITE_BALANCE_AUTO = 0
+    #OV5640_WHITE_BALANCE_SUNNY = 1
+    #OV5640_WHITE_BALANCE_FLUORESCENT = 2
+    #OV5640_WHITE_BALANCE_CLOUDY = 3
+    #OV5640_WHITE_BALANCE_INCANDESCENT = 4
+
+    cam.flip_y = False
+    cam.flip_x = False
+    #cam.night_mode = False
+
+    #cam.exposure_value = 0
+    #cam.brightness = 0
+    #cam.saturation = 0
+    #cam.contrast = 0
+    # In test bar mode, the camera shows color bars in the order white - yellow - cyan - green - purple - red - blue - black.
+    cam.test_pattern = False
+    
+    print("Resolution info:")
     (
         w,
         h,
         ratio) = adafruit_ov5640._resolution_info[cam.size]
-
     print("w = {}".format(w))
     print("h = {}".format(h))
     print("ratio = {}".format(ratio))
 
-    # Can I access the values in ratio table?
-    (
-        max_width,
-        max_height,
-        start_x,
-        start_y,
-        end_x,
-        end_y,
-        offset_x,
-        offset_y,
-        total_x,
-        total_y,
-    ) = adafruit_ov5640._ratio_table[ratio]
-    print("max_width = {}".format(max_width))
-    print("max_height = {}".format(max_height))
-    print("start_x = {}".format(start_x))
-    print("start_y = {}".format(start_y))
-    print("end_x = {}".format(end_x))
-    print("end_y = {}".format(end_y))
-    print("offset_x = {}".format(offset_x))
-    print("offset_y = {}".format(offset_y))
-    print("total_x = {}".format(total_x))
-    print("total_y = {}".format(total_y))
-
     print("\nRegistry values\n")
-    p_size = cam._size
+    p_size = cam.size
     p_width, p_height, p_ratio = adafruit_ov5640._resolution_info[p_size]
     (
         p_max_width,
@@ -112,88 +161,4 @@ def change_setting(cam):
 
     print("p_write_reg_bits(_ISP_CONTROL_01, 0x20, {})".format(p_scale))
 
-
-def init_camera():
-
-    # Change the values in row 0 of the _resolution_info table
-    #adafruit_ov5640._resolution_info[0] = [2560,1400,8]  # Assign new values to row 0
-    # Change the values in row 0 of the _ratio_table table
-
-    #  mw,   mh,  sx,  sy,   ex,   ey, ox, oy,   tx,   ty
-    #adafruit_ov5640._ratio_table[0] =  [1920, 1920, 320, 0, 2543, 1951, 32, 16, 2684, 1968]  # 1x1
-    #adafruit_ov5640._ratio_table[0] = [2650, 1820, 850, 500, 1800, 1400, 32, 16, 2844, 1868]  # Assign new values to row 0
-
-    print("initialising Camera")
-    print("construct bus")
-    bus = busio.I2C(board.GP9, board.GP8)
-    print("construct camera")
-    reset = digitalio.DigitalInOut(board.GP10)
-    cam = adafruit_ov5640.OV5640(
-    bus,
-    data_pins=(
-        board.GP12,
-        board.GP13,
-        board.GP14,
-        board.GP15,
-        board.GP16,
-        board.GP17,
-        board.GP18,
-        board.GP19,
-    ),
-    clock=board.GP11,
-    vsync=board.GP7,
-    href=board.GP21,
-    mclk=board.GP20,
-    shutdown=None,
-    reset=reset,
-    size=adafruit_ov5640.OV5640_SIZE_96X96
-    )
-    print("print chip id")
-    print(cam.chip_id)
-
-    #OV5640_SIZE_SVGA = 9  # 800x600
-    #OV5640_SIZE_XGA = 10  # 1024x768
-    #OV5640_SIZE_HD = 11  # 1280x720
-    #OV5640_SIZE_SXGA = 12  # 1280x1024
-    #OV5640_SIZE_UXGA = 13  # 1600x1200
-    #OV5640_SIZE_QHDA = 14  # 2560x1440
-    #OV5640_SIZE_WQXGA = 15  # 2560x1600
-    #OV5640_SIZE_PFHD = 16  # 1088x1920
-    #OV5640_SIZE_QSXGA = 17  # 2560x1920
-
-    # This sets the color format for the camera to RGB.
-    # The constant adafruit_ov5640.OV5640_COLOR_RGB specifies the byte ordering and format of the color data that the OV5640 camera outputs.
-    # In this case, the format is RGB with little-endian byte ordering.
-    #cam.colorspace = adafruit_ov5640.OV5640_COLOR_RGB
-    
-    # 2560x1440 @ 24 seems like a good starting point
-
-    # Use JPG
-    cam.quality = 24
-    cam.colorspace = adafruit_ov5640.OV5640_COLOR_JPEG 
-
-    # Use RGB
-    #cam.colorspace = adafruit_ov5640.OV5640_COLOR_JPEG 
-    
-    cam.effect = adafruit_ov5640.OV5640_SPECIAL_EFFECT_NONE
-    #cam.effect = adafruit_ov5640.OV5640_SPECIAL_EFFECT_NEGATIVE
-    cam.white_balance = adafruit_ov5640.OV5640_WHITE_BALANCE_AUTO
-
-    #OV5640_WHITE_BALANCE_AUTO = 0
-    #OV5640_WHITE_BALANCE_SUNNY = 1
-    #OV5640_WHITE_BALANCE_FLUORESCENT = 2
-    #OV5640_WHITE_BALANCE_CLOUDY = 3
-    #OV5640_WHITE_BALANCE_INCANDESCENT = 4
-
-    cam.flip_y = False
-    cam.flip_x = False
-    #cam.night_mode = False
-
-    #cam.exposure_value = 0
-    #cam.brightness = 0
-    #cam.saturation = 0
-    #cam.contrast = 0
-    # In test bar mode, the camera shows color bars in the order white - yellow - cyan - green - purple - red - blue - black.
-    cam.test_pattern = False
-    
     return cam
